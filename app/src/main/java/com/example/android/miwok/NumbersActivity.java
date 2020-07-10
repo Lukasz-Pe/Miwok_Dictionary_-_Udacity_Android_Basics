d3b6e0f97de5e303c6cd73f2e0b1208f0d81a95b
Łukasz Pękalski <lukaszpe@gmail.com>
2020-07-10 13:57:15 +0200

    feature: Items are displayed as TextViews

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +30,17 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words=new ArrayList<String>();
+        words.addAll(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
+
+        LinearLayout rootView=(LinearLayout)findViewById(R.id.numbersListRoot);
+
+        for (int i=0;i<words.size();i++){
+            TextView wordView=new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
     }
 }
