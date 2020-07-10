ef1cc90203c6ee3a8a13ac64126c6a3749c1f45d
Łukasz Pękalski <lukaszpe@gmail.com>
2020-07-10 14:18:20 +0200

    feature: Items are displayed as ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,13 +36,9 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<String> words=new ArrayList<String>();
         words.addAll(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
 
-        LinearLayout rootView=(LinearLayout)findViewById(R.id.numbersListRoot);
-
-        for (int i=0;i<words.size();i++){
-            TextView wordView=new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-        }
+        ArrayAdapter<String> wordsArray = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+        ListView listView=(ListView) findViewById(R.id.list);
+        listView.setAdapter(wordsArray);
 
     }
 }
